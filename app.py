8e994ca931e6e472946b65329083f9afbde3cdee
Phat8284 <ducphatnd2007@gmail.com>
2026-02-06 05:11:41 +0000

    khoi tao

---
+++ app.py
@@ -0,0 +1,30 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+
+@app.route("/")
+
+def hello_world():
+
+    return "<p>Hello, World!</p>"
+
+
+
+@app.route("/hello")
+def hello():
+    return render_template('index.html')
+
+
+@app.route('/test')
+def test():
+    return render_template('hello.html')
+
+
+
+if __name__ == '__main__':
+
+    # Chạy ứng dụng ở chế độ debug
+
+    app.run(debug=True, port=9000)
